445ab3c34b86a83807901fa5bab8c914b0a1b241
oiroe <pathiti.b@gmail.com>
2023-09-19 02:08:39 +0700

    echo.c

--- src/echo.c
+++ src/echo.c
@@ -12,7 +12,38 @@
 
 #include "minishell.h"
 
-void	echo_fun(char **)
+int	argv_count(char **av)
 {
+	int	i;
 
+	i = 0;
+	while (av[i])
+		i++;
+	return (i);
+}
+
+void	echo_fun(char **argv)
+{
+	int	i;
+	int	new_line;
+	int	count;
+
+	new_line = 1;
+	i = 1;
+	count = argv_count(argv);
+	if (str[i] && ft_strnstr(argv[i], "-n", 2))
+	{
+		i++;
+		new_line = 0;
+	}
+	while (argv[i])
+	{
+		printf("%s", argv[i]);
+		if (argv[i] && i != count - 1)
+			printf(" ");
+		i++;
+	}
+	if (!argv[i] && !new_line)
+		printf("\n");
+	return (EXIT_SUCCESS);
 }
