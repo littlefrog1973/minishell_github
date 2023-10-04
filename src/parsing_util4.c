e315c7a82e6da5029ea3d3f09804996378592599
Sukit Deeyiengyang <kitdee@hotmail.com>
2023-10-04 23:06:19 +0700

    corrected util4

--- src/parsing_util4.c
+++ src/parsing_util4.c
@@ -6,11 +6,23 @@
 /*   By: sdeeyien <sukitd@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 23:00:19 by sdeeyien          #+#    #+#             */
-/*   Updated: 2023/10/04 22:41:12 by sdeeyien         ###   ########.fr       */
+/*   Updated: 2023/10/04 23:05:49 by sdeeyien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	set_lst_file(char *cmd, t_file *infile)
+{
+	while (1)
+	{
+		ft_memset(ft_strnstr(cmd, infile->filename, ft_strlen(cmd)),
+			' ', ft_strlen(infile->filename));
+		if (infile->next == NULL)
+			break ;
+		infile = infile->next;
+	}
+}
 
 char	*del_in_out2(char *ccmd, t_file *infile, t_file *outfile)
 {
@@ -22,27 +34,9 @@ char	*del_in_out2(char *ccmd, t_file *infile, t_file *outfile)
 	if (!cmd)
 		return (perror("parsing"), NULL);
 	if (infile)
-	{
-		while (1)
-		{
-			ft_memset(ft_strnstr(cmd, infile->filename, ft_strlen(cmd)),
-				' ', ft_strlen(infile->filename));
-			if (infile->next == NULL)
-				break ;
-			infile = infile->next;
-		}
-	}
+		set_lst_file(cmd, infile);
 	if (outfile)
-	{
-		while (1)
-		{
-			ft_memset(ft_strnstr(cmd, outfile->filename, ft_strlen(cmd)),
-				' ', ft_strlen(outfile->filename));
-			if (outfile->next == NULL)
-				break ;
-			outfile = outfile->next;
-		}
-	}
+		set_lst_file(cmd, outfile);
 	i = -1;
 	while (cmd[++i])
 		if (cmd[i] == '<' || cmd[i] == '>')
