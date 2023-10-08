1ea5125c0db9ee648c3fbc451a0f2d7b023acd5b
Sukit Deeyiengyang <kitdee@hotmail.com>
2023-10-08 07:31:36 +0700

    revise set_lst_file to mark a proper filename by add ft_strchr

--- src/parsing_util4.c
+++ src/parsing_util4.c
@@ -6,17 +6,20 @@
 /*   By: sdeeyien <sukitd@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 23:00:19 by sdeeyien          #+#    #+#             */
-/*   Updated: 2023/10/06 08:18:32 by sdeeyien         ###   ########.fr       */
+/*   Updated: 2023/10/08 07:26:14 by sdeeyien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	set_lst_file(char *cmd, t_file *infile)
+static void	set_lst_file(char *cmd, t_file *infile, char arrow)
 {
+	size_t	i;
+
 	while (1)
 	{
-		ft_memset(ft_strnstr(cmd, infile->filename, ft_strlen(cmd)),
+		i = ft_strchr(cmd, arrow) - cmd;
+		ft_memset(ft_strnstr(&cmd[i], infile->filename, ft_strlen(&cmd[i])),
 			' ', ft_strlen(infile->filename));
 		if (infile->next == NULL)
 			break ;
@@ -34,9 +37,9 @@ char	*del_in_out2(char *ccmd, t_file *infile, t_file *outfile)
 	if (!cmd)
 		return (perror("parsing"), NULL);
 	if (infile)
-		set_lst_file(cmd, infile);
+		set_lst_file(cmd, infile, '<');
 	if (outfile)
-		set_lst_file(cmd, outfile);
+		set_lst_file(cmd, outfile, '>');
 	i = -1;
 	while (cmd[++i])
 		if (cmd[i] == '<' || cmd[i] == '>')
